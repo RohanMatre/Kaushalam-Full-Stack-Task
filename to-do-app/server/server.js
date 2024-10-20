f0ca380e59e7da25a5deee58b882913db797e8ca
RohanMatre <rohan.mce21@sot.pdpu.ac.in>
2024-10-20 12:17:42 +0530

    Setup Server.js & PostgresDB

---
+++ to-do-app/server/server.js
@@ -0,0 +1,18 @@
+const PORT = process.env.PORT ?? 8000
+const express = require('express')
+const app = express()
+const pool = require('./db')
+
+// Get all todos
+app.get('/todos', async (req, res) => {
+    const userEmail = 'rohan@test.com'
+
+    try{
+        const todos = await pool.query('SELECT * FROM todo WHERE user_email = $1', [userEmail])
+        res.json(todos.rows)
+    } catch (error) {   
+        console.log(error)
+    }
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
