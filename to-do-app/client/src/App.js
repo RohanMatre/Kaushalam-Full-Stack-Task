bc2b453f62f880881daae1ee0f654e5bc2e7cff4
RohanMatre <rohan.mce21@sot.pdpu.ac.in>
2024-10-20 21:20:18 +0530

    Add dependencies and implement authentication

--- to-do-app/client/src/App.js
+++ to-do-app/client/src/App.js
@@ -1,34 +1,56 @@
-import ListHeader from './components/ListHeader'
-import ListItem from './components/ListItem'
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
+import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
+import Auth from "./components/Auth";
+import { useCookies } from "react-cookie";
 
 const App = () => {
-  const userEmail = 'rohan@test.com'
-  const [tasks, setTasks] = useState(null)
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const authToken = cookies.AuthToken
+  const userEmail = cookies.Email
+  const username = userEmail ? userEmail.split('@')[0] : '';
+  const capitalizedUsername = username ? username.charAt(0).toUpperCase() + username.slice(1) : '';
+  const [tasks, setTasks] = useState(null);
 
   const getData = async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
-      const json = await response.json()
-      setTasks(json)
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`
+      );
+      const json = await response.json();
+      setTasks(json);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  useEffect(() => getData, [])
-  console.log(tasks)
+  useEffect(() => {
+    if (authToken) {
+      getData();
+    }
+  }, []);
+  console.log(tasks);
 
   // Sort by date
-  const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date))
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
 
   return (
     <div className="app">
-      <ListHeader listName = {'✍🏻 Daily To-Do List'} getData={getData} />
-      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
+    {!authToken && <Auth />}
+      {authToken && (
+        <>
+          <ListHeader listName={"✍🏻 Daily To-Do List"} getData={getData} />
+          <p className="user-email">Welcome back {capitalizedUsername}</p>
+          {sortedTasks?.map((task) => (
+            <ListItem key={task.id} task={task} getData={getData} />
+          ))}
+        </>
+      )}
+      <p className="copyright"> © Rohan Matre • 2024</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- to-do-app/client/src/components/Auth.js
+++ to-do-app/client/src/components/Auth.js
@@ -1,8 +1,128 @@
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+
 const Auth = () => {
-    return (
-      <div>
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [isLogIn, setIsLogin] = useState(true);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [error, setError] = useState(null);
+
+  console.log(cookies);
+
+  const viewLogin = (status) => {
+    setError(null);
+    setIsLogin(status);
+  };
+
+  const handleSubmit = async (e, endpoint) => {
+    e.preventDefault();
+    setError(null);
+
+    // Check for missing fields or mismatched passwords
+    if (!email || !password) {
+      setError("Please fill all the fields");
+      return;
+    }
+    if (!isLogIn && password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/${endpoint}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      // Handle different response statuses
+      if (response.status === 200) {
+        setError("User already exists");
+      }
+      if (response.status === 201) {
+        setError("User created successfully");
+      }
+      if (response.status === 202) {
+        setError("User logged in successfully");
+      }
+
+      // Safely await the JSON response
+      const data = await response.json();
+      if (data.detail) {
+        setError(data.detail);
+      } else {
+        setCookie("Email", data.email);
+        setCookie("AuthToken", data.token);
+
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          <h2>{isLogIn ? "Log In" : "Sign Up"}</h2>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {!isLogIn && (
+            <input
+              type="password"
+              placeholder="confirm password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          )}
+          <input
+            type="submit"
+            className="create"
+            onClick={(e) => handleSubmit(e, isLogIn ? "login" : "signup")}
+          />
+          {error && <p>{error}</p>}
+        </form>
+        <div className="auth-options">
+          <button
+            onClick={() => viewLogin(false)}
+            style={{
+              backgroundColor: !isLogIn
+                ? "rgb(255,255,255)"
+                : "rgb(188,188,188)",
+            }}
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={() => viewLogin(true)}
+            style={{
+              backgroundColor: isLogIn
+                ? "rgb(255,255,255)"
+                : "rgb(188,188,188)",
+            }}
+          >
+            Login
+          </button>
+        </div>
       </div>
-    )
-  }
-  
-  export default Auth
+    </div>
+  );
+};
+
+export default Auth;
